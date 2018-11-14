a3ea75fec347b039263b69a0da57dfb91c70554c
moon0920 <moon0920@dreamwiz.com>
2018-11-14 10:51:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/edu/newmediaplayer/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.edu.newmediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    MediaPlayer mPlayer;
+    Button btnStop, btnPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnback = findViewById(R.id.btnback);
+        btnback.setOnClickListener(this);
+        btnStop = (Button)findViewById(R.id.btnStop);
+        btnStop.setOnClickListener(this);
+        btnPlay = (Button)findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(this);
+        Button btnFast = findViewById(R.id.btnFast);
+        btnFast.setOnClickListener(this);
+        mPlayer = MediaPlayer.create(this,R.raw.thunder_rain);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnPlay:
+                mPlayer.start();
+                btnPlay.setEnabled(true);
+                btnStop.setEnabled(false);
+            break;
+            case R.id.btnStop:
+                mPlayer.pause();
+                btnStop.setEnabled(true);
+                btnPlay.setEnabled(false);
+                break;
+
+        }
+
+    }
+}
